38a96f46bd9443174b1d1d85cedcfc6554c69f06
Manish <mkc1020304050@gmail.com>
2023-08-04 17:48:30 +0530

    input auto focus

--- client/src/components/Infobar/Infobar.js
+++ client/src/components/Infobar/Infobar.js
@@ -1,16 +1,14 @@
 import React from "react";
 import './Infobar.css'
 import closeIcon from '../icons/closeIcon.png'
-import onlineIcon from '../icons/onlineIcon.png'
 
 const Infobar = ({room}) => {
   return (
     <div className="infoBar">
         <div className="leftInnerConatiner">
             
-        <div><img className="onlineIcon" src={onlineIcon} /></div>
-            <div><h3>{room}</h3></div>
-            </div>
+        <h3>You are in :{room}</h3></div>
+        
         
         <div className="rightInnerContainer">
             <a href="/"><img src={closeIcon} alt="close image"/></a>
--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import './Input.css'
 
 
 const Input = ({message,setMessage,sendMessage}) => {
+  const myRef = useRef();
+  function focus(myRef) {
+    // setTimeout(() => { myRef.current?.focus() }, 50);
+    myRef.current?.focus()
+  }
+
+  useEffect(() => { focus(myRef) },[])
   return (
     <form className="form">
     <input
+    ref={myRef}
         className="input"
         type="text"
         placeholder="Type your message..."
         value={message}
         onChange={(event) => setMessage(event.target.value)} 
-        onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}
+        onKeyUp={(event) => event.key === 'Enter' ? sendMessage(event) : null}
     />
     <button className="sendButton" onClick={(event)=> sendMessage(event)}>Send</button>
 </form>
