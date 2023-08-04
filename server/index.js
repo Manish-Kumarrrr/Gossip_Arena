91f2312934be302c42e0342d8d3f6c2f3039271f
Manish-Kumarrrr <118111377+Manish-Kumarrrr@users.noreply.github.com>
2023-08-05 02:12:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
 
   cors: {
-    origin: "http://localhost:3000"
+    origin: "https://gossip-arena-ndsg.vercel.app/"
   }
 });
 
