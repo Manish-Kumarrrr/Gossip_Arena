23699847ae8812178336d9065b48a662e2e99aea
Manish-Kumarrrr <118111377+Manish-Kumarrrr@users.noreply.github.com>
2023-08-05 11:30:43 +0530

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -10,50 +10,50 @@ import { Server } from "socket.io";
 const app = express();
 app.use('/', router); 
 const httpServer = createServer(app);
-const io = new Server(httpServer
-                      ,{
+// const io = new Server(httpServer
+//                       ,{
 
-  cors: {
-    origin: "https://gossip-arena-ndsg.vercel.app/"
-  }
-}
-                     );
+//   cors: {
+//     origin: "https://gossip-arena-ndsg.vercel.app/"
+//   }
+// }
+//                      );
 
 // app.use(cors());
 
-io.on("connection", (socket) => {
-  console.log("we have a new connection!!!");
+// io.on("connection", (socket) => {
+//   console.log("we have a new connection!!!");
 
-  socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
-    const { error, user } = addUser({ id: socket.id, name, room });
+//   socket.on('join', ({ name, room }, callback) => {
+//     console.log(name, room);
+//     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) return callback(error);
+//     if (error) return callback(error);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room} 🤙🤟` })
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name},has joined` });
-    socket.join(user.room);
+//     socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room} 🤙🤟` })
+//     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name},has joined` });
+//     socket.join(user.room);
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+//     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
     
 
-  });
+//   });
 
-  socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
+//   socket.on('sendMessage', (message, callback) => {
+//     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message });
-    io.to(user.room).emit('roomData', { room: user.room, users:getUsersInRoom(user.room) });
+//     io.to(user.room).emit('message', { user: user.name, text: message });
+//     io.to(user.room).emit('roomData', { room: user.room, users:getUsersInRoom(user.room) });
    
-  });
+//   });
 
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
-    if (user) {
-      io.to(user.room).emit('message', { user: "admin", text: `${user.name} has left.👋👋` })
-    }
-  })
-});
+//   socket.on('disconnect', () => {
+//     const user = removeUser(socket.id);
+//     if (user) {
+//       io.to(user.room).emit('message', { user: "admin", text: `${user.name} has left.👋👋` })
+//     }
+//   })
+// });
 
 httpServer.listen(PORT, () => console.log("server is starting on port ", PORT));
