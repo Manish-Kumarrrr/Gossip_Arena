b24cf8126a4b5900586177417546921ad487c3a6
Manish <mkc1020304050@gmail.com>
2023-08-10 22:29:05 +0530

    input box become blank after sending message-2

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -2,27 +2,34 @@ import React, { useEffect, useRef } from "react";
 import './Input.css'
 
 
-const Input = ({message,setMessage,sendMessage}) => {
+const Input = ({ message, setMessage, sendMessage }) => {
   const myRef = useRef();
   function focus(myRef) {
     // setTimeout(() => { myRef.current?.focus() }, 50);
     myRef.current?.focus()
   }
 
-  useEffect(() => { focus(myRef) },[message])
+  useEffect(() => { focus(myRef) }, [message])
+
   return (
     <form className="form">
-    <input
-    ref={myRef}
+      <input
+        ref={myRef}
         className="input"
         type="text"
         placeholder="Type your message..."
         value={message}
-        onChange={(event) => setMessage(event.target.value)} 
-        onKeyUp={(event) => {event.key === 'Enter' ? sendMessage(event): null; setMessage('');}}
-    />
-    <button className="sendButton" onClick={(event)=> sendMessage(event)}>Send</button>
-</form>
+        onChange={(event) => setMessage(event.target.value)}
+        onKeyUp={(event) => { 
+          // (event.key === 'Enter') ? sendMessage(event) : null; 
+          if (event.key === "Enter"){
+            sendMessage(event);
+            setMessage(''); 
+          }
+        }}
+      />
+      <button className="sendButton" onClick={(event) => { sendMessage(event); setMessage('') }}>Send</button>
+    </form>
   )
 }
 
